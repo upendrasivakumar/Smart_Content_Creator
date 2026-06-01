aba0e227adfc71c01a4755d2190e297778bb6dc3
upendrasivakumar <163397132+upendrasivakumar@users.noreply.github.com>
2026-06-01 16:43:23 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -7,16 +7,9 @@ load_dotenv()
 
 app = FastAPI()
 
-api_key = os.getenv("GROQ_API_KEY")
-
-if not api_key:
-    raise Exception("GROQ_API_KEY not found")
-
-client = Groq(api_key=api_key)
-
-@app.get("/")
-def home():
-    return {"message": "API Running"}
+client = Groq(
+    api_key=os.getenv("api_key")
+)
 
 @app.post("/generate")
 def generate_content(
@@ -25,6 +18,7 @@ def generate_content(
     content_type: str,
     tone: str
 ):
+
     prompt = f"""
     Generate a {content_type}
 
